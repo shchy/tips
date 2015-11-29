0410e7ad619d76d458dd235a35146cf299c746fc
shch <shch.pc@gmail.com>
2015-11-29 21:33:46 +0900

    stash

---
+++ Tips.Model/Models/IProject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tips.Model.Models
+{
+    public interface IProject : IIdentity<int>
+    {
+        string Name { get; }
+
+    }
+
+
+
+
+    public interface IStoryBoard
+    {
+
+
+    }
+
+
+}
--- Tips.Web/Modules/UserModule.cs
+++ Tips.Web/Modules/UserModule.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Nancy;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace Tips.Web.Modules
 {
-    class UserModule
+    public class UserModule : NancyModule
     {
+        public UserModule()
+        {
+
+        }
     }
 }
