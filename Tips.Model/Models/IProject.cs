17b59dc2786e9248b59fdeb6ad3ef05519d77e1d
横田 修一 <yokota.shuichi@cy-com.co.jp>
2016-01-25 17:30:21 +0900

    add desktop

--- Tips.Model/Models/IProject.cs
+++ Tips.Model/Models/IProject.cs
@@ -6,20 +6,70 @@ using System.Threading.Tasks;
 
 namespace Tips.Model.Models
 {
-    public interface IProject : IIdentity<int>
+    public interface INameable
     {
         string Name { get; }
-
     }
 
 
 
 
-    public interface IStoryBoard
+    public interface IResource : IIdentity<int>, INameable
     {
-
-
+        /// <summary>
+        /// V/H
+        /// </summary>
+        double Cost { get; }
     }
+
+    public interface IRange<T>
+    {
+        T Left { get; }
+        T Right { get; }
+    }
+
+    public interface ITask : IIdentity<int>, INameable
+    {
+        double Value { get; }
+    }
+
+    public interface ITaskRecord : IIdentity<int>
+    {
+        int TaskId { get; }
+        DateTime Day { get; }
+        double Value { get; }
+        double WorkValue { get; }
+        IResource Who { get; }
+    }
+
+    //public interface ITaskWithRecord 
+    //{
+    //    ITask Task { get; }
+    //    IEnumerable<ITaskRecord> Records { get;}
+    //}
+
+    public interface IPlan : IIdentity<int>
+    {
+        DateTime Day { get; }
+        double Value { get; }
+        IResource Who { get; }
+    }
+
+    public interface IItelator : IIdentity<int>, INameable
+    {
+        IEnumerable<ITask> Tasks { get; }
+        IEnumerable<ITaskRecord> Records { get; }
+        IEnumerable<IPlan> Plans { get; }
+    }
+
+    public interface IProject : IIdentity<int>, INameable
+    {
+        IEnumerable<IItelator> Itelators { get; }
+    }
+
+
+
+
 
 
 }
