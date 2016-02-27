122cc1d4b0874cd1e7277bd40a8e48ace6c6075c
横田 修一 <yokota.shuichi@cy-com.co.jp>
2016-02-27 20:49:52 +0900

    commit 漏れ

---
+++ Tips.WebServer/content/js/tips.js
@@ -0,0 +1,52 @@
+
+
+
+function getJson(url, callback){
+  ajaxJson(url, 'get', {}, callback);
+}
+
+function getJson(url, jsonData, callback){
+  ajaxJson(url, 'get', jsonData, callback);
+}
+
+function postJson(url, jsonData, callback)
+{
+  ajaxJson(url,'post', jsonData, callback);
+}
+
+function deleteJson(url, jsonData, callback)
+{
+  ajaxJson(url,'delete', jsonData, callback);
+}
+
+function ajaxJson(url, method, jsonData, callback){
+  var json = JSON.stringify(jsonData);
+  $.ajax({
+    type:method,
+    url:url,
+    data : json,
+    dataType : 'JSON',
+    scriptCharset: 'utf-8',
+    success : callback,
+    error : function(data) {
+        // Error
+        alert("error");
+        alert(JSON.stringify(data));
+    }
+  });
+}
+
+function readFile(file, callback)
+{
+  var isEnable=file.files && file.files[0];
+  if ( isEnable == undefined ) {
+    callback();
+  }
+
+  var r = new FileReader();
+  r.onload = function(e) {
+    var base64String = e.target.result;
+    callback(base64String.split(',')[1]);
+  };
+  r.readAsDataURL( file.files[0] );
+}
